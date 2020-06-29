56e0c07e9c845d67e9e102dcc641bba19dcc27f8
Lucas Ferreira <blooodcf@gmail.com>
2020-06-29 07:11:22 -0300

    Sequelize setup

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  dialect: process.env.DB_DIALECT,
+  operatorAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
