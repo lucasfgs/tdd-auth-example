b09aae4667157179dc97e20081b1d1e4963da632
Lucas Ferreira <blooodcf@gmail.com>
2020-06-29 06:51:05 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+require("dotenv").config({
+  path: process.env.NODE_ENV == "test" ? ".env-test" : ".env",
+});
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.listen(process.env.PORT, () =>
+  console.log(`Listening at port: ${process.env.PORT}`)
+);
